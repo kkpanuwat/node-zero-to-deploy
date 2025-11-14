8b3b602997153b9359108a4682d946a0ea37a769
kkpanuwat <panuwat_ys@kkumail.com>
2025-11-14 21:08:17 +0700

    feature:new module 1.1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Kcode Doc',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
   stylesheets: [
@@ -77,9 +77,9 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Kcode Doc',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Kcode Doc Logo',
         src: 'img/logo.svg',
       },
       items: [

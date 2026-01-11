8fdac3eec954159b00b5c93134aaf9b0af6521b4
kkpanuwat <panuwat_ys@kkumail.com>
2026-01-11 15:43:10 +0700

    chore: remove day 1 progress summary

--- sidebars.ts
+++ sidebars.ts
@@ -43,8 +43,7 @@ const sidebars: SidebarsConfig = {
               ],
             },
             'nodejs-zero-to-hero/day-1-introduction/day-1-git-basics',
-            'nodejs-zero-to-hero/day-1-introduction/day-1-hands-on-labs',
-            'nodejs-zero-to-hero/day-1-introduction/day-1-progress-summary',
+            'nodejs-zero-to-hero/day-1-introduction/day-1-hands-on-labs'
           ],
         },
       ],
